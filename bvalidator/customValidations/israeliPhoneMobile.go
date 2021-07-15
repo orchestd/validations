b7d7bc005b206162ae1010bf70b63eaf96fa2435
Stav Gayer <stav.g@heilasystems.com>
2021-07-15 10:20:50 +0300

    custom validations support

---
+++ bvalidator/customValidations/israeliPhoneMobile.go
@@ -0,0 +1,18 @@
+package customValidations
+
+import (
+	"github.com/go-playground/validator"
+	"regexp"
+)
+
+// validate phone number "0501234567"
+func ValidateIsraeliPhoneMobile(fl validator.FieldLevel) bool {
+	re := regexp.MustCompile("^[0][5][0|1|2|3|4|5|6|7|8|9]{1}[0-9]{7}$")
+	fv := fl.Field().String()
+
+	// allow empty string if fields is not a required field
+	if re.MatchString(fv) || fv == "" {
+		return true
+	}
+	return false
+}
---
+++ bvalidator/customValidations/longDate.go
@@ -0,0 +1,18 @@
+package customValidations
+
+import (
+	"github.com/go-playground/validator"
+	"regexp"
+)
+
+// validate datetime format "2009-11-10 23:00"
+func ValidateLongDate(fl validator.FieldLevel) bool {
+	re := regexp.MustCompile("[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]")
+	fv := fl.Field().String()
+
+	// allow empty string if fields is not a required field
+	if re.MatchString(fv) || fv == "" {
+		return true
+	}
+	return false
+}
