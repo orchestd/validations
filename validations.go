b8b8cc732e2988d88370c2e8502bff16e74648e0
Andrey Mikheev <mixe.andrey@gmail.com>
2021-11-21 13:53:56 +0200

    added validatior getter

--- validations.go
+++ validations.go
@@ -1,6 +1,12 @@
 package validations
 
-import "bitbucket.org/HeilaSystems/servicereply"
+import (
+	"bitbucket.org/HeilaSystems/dependencybundler/interfaces/cache"
+	"bitbucket.org/HeilaSystems/servicereply"
+	"context"
+	"sort"
+	"sync"
+)
 
 type CustomValidation interface {
 	GetValidationTag() string
@@ -9,9 +15,68 @@ type CustomValidation interface {
 
 type Builder interface {
 	AddCustomValidations(...CustomValidation) Builder
-	Build() (Validations,error)
+	Build() (Validations, error)
 }
 type Validations interface {
 	Validate(req interface{}) servicereply.ServiceReply
 }
 
+type Validator interface {
+	Validate() error
+	GetId() string
+	GetName() string
+	SetLevel(string)
+	SetMessage(string)
+}
+
+type CacheValidator struct {
+	Id      string
+	Level   string
+	Message string
+	SortOrder    int
+}
+
+func NewCacheValidatorGetter(cache cache.CacheStorageGetter) ValidatorGetter {
+	return &cacheValidatorGetter{cache: cache}
+}
+
+type ValidatorGetter interface {
+	GetValidators(c context.Context, initValidators []Validator) ([]Validator, error)
+}
+
+type cacheValidatorGetter struct {
+	cache           cache.CacheStorageGetter
+	cacheValidators []CacheValidator
+	sync.Mutex
+}
+
+func (v *cacheValidatorGetter) GetValidators(c context.Context, initValidators []Validator) ([]Validator, error) {
+	v.Lock()
+	defer v.Unlock()
+	if v.cacheValidators == nil {
+		validators := make(map[string]CacheValidator)
+		err := v.cache.GetAll(c, "validators", "1", validators)
+		if err != nil {
+			return nil, err
+		}
+		var cacheValidators []CacheValidator
+		for _, v := range validators {
+			cacheValidators = append(cacheValidators, v)
+		}
+		sort.Slice(cacheValidators, func(i, j int) bool {
+			return cacheValidators[i].SortOrder < cacheValidators[j].SortOrder
+		})
+		v.cacheValidators = cacheValidators
+	}
+	var validators []Validator
+	for _, cv := range v.cacheValidators {
+		for _, iv := range initValidators {
+			if iv.GetId() == cv.Id {
+				iv.SetLevel(cv.Level)
+				iv.SetMessage(cv.Message)
+				validators = append(validators, iv)
+			}
+		}
+	}
+	return validators, nil
+}
